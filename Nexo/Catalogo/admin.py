2f614d53ea89e2bc030b0b8a562998876e77b66a
Ubuntu <ubuntu@ip-172-31-13-222.eu-central-1.compute.internal>
2022-11-26 17:02:30 +0000

    Admin

--- Nexo/Catalogo/admin.py
+++ Nexo/Catalogo/admin.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 from .models import Libro, Pelicula, Serie
 
 # Register your models here.
-admin.site.register(Libro)
-admin.site.register(Pelicula)
-admin.site.register(Serie)
+@admin.register(Libro)
+class LibroAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "genero", "leido", "puntuacion")
+    
+@admin.register(Pelicula)
+class LibroAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "genero", "vista", "puntuacion")
+    
+@admin.register(Serie)
+class LibroAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "genero", "vista", "puntuacion")
